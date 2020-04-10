0fb43ecb9e2e617174135fcf989ab7248cf05d7e
JuanJose <42131930+AllMigth@users.noreply.github.com>
2020-04-10 00:53:38 -0500

    add helmet to the title and content

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react';
 import Nav from './Nav';
+import { Helmet } from 'react-helmet'
 
 export default (props) => {
     return(
         <Fragment>
+            <Helmet title="SoftYon Store">
+                <meta name="description" content="Super cool store to add SWAG de SoftYon"></meta>
+            </Helmet>
             <Nav/>
             <main> {props.children} </main>
         </Fragment>
